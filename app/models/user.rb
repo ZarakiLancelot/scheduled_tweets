92e8a894fc5cb92e1017e5a3c30e210a4e172011
ZarakiLancelot <kenpachi2783@gmail.com>
2022-07-05 23:45:01 -0600

    validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,15 +2,14 @@
 #
 # Table name: users
 #
-#  id                      :integer          not null, primary key
-#  email                   :string
-#  password_digest         :string
-#  created_at              :datetime         not null
-#  updated_at              :datetime         not null
-#
-#  password                :string virtual
-#  password_confirmation   :string virtual
+#  id              :integer          not null, primary key
+#  email           :string           not null
+#  password_digest :string
+#  created_at      :datetime         not null
+#  updated_at      :datetime         not null
 #
 class User < ApplicationRecord
     has_secure_password
+
+    validates :email, presence: true, format: { with: /\A[^@\s]+[^@\s]+\z/, message: "Debe ser un correo electrónico válido!" }
 end
--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -3,7 +3,7 @@
 # Table name: users
 #
 #  id              :integer          not null, primary key
-#  email           :string
+#  email           :string           not null
 #  password_digest :string
 #  created_at      :datetime         not null
 #  updated_at      :datetime         not null
