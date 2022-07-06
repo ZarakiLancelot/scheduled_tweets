6312607cf9990e3a45dc7bd8d9c57d19f30c9d15
ZarakiLancelot <kenpachi2783@gmail.com>
2022-07-06 00:24:08 -0600

    creating a sign up form

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
   get "about", to: "about#index"
 
+  get "sign_up", to: "registration#new"
+  post "sign_up", to: "registration#create"
+
   root to: "main#index"
 end
