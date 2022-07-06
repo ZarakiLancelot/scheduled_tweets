e624967a51728a2e847be402ea82529d3ed5078d
ZarakiLancelot <kenpachi2783@gmail.com>
2022-07-05 22:44:51 -0600

    flash messages

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -1,6 +1,8 @@
 class MainController < ApplicationController
 
     def index
+        flash.now[:notice] = "Inicio de sesión exitosa!"
+        flash.now[:alert] = "Correo o contraseña incorrectos!"
     end
 
 end
