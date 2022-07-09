02d2dbee8c737d26e39cf9b4c675bd6de23f5e1e
ZarakiLancelot <kenpachi2783@gmail.com>
2022-07-09 01:58:51 -0600

    logging out

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
 
   get "sign_up", to: "registration#new"
   post "sign_up", to: "registration#create"
+  delete "logout", to: "sessions#destroy"
 
   root to: "main#index"
 end
