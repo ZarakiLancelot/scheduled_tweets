63cbd0956d029cd332ab16e5cbb515c51d29cdfb
ZarakiLancelot <kenpachi2783@gmail.com>
2022-07-10 07:32:37 -0600

    accessing the current user

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -1,13 +1,4 @@
 class MainController < ApplicationController
-
     def index
-        
-        if session[:user_id]
-            @user = User.find_by(id: session[:user_id])
-            # flash.now[:notice] = "Inicio de sesión exitosa!"
-        else
-            # flash.now[:alert] = "Correo o contraseña incorrectos!"
-        end
     end
-
 end
