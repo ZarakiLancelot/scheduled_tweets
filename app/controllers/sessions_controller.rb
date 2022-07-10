d66a6ca59154834fa3a64396d3fdbe4e8c997ffc
ZarakiLancelot <kenpachi2783@gmail.com>
2022-07-10 07:15:17 -0600

    login form

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,4 +1,19 @@
 class SessionsController < ApplicationController
+    def new
+    end
+
+    def create
+        user = User.find_by(email: params[:email])
+
+        if user.present? && user.authenticate(params[:password])
+            session[:user_id] = user.id
+            redirect_to root_path, notice: "Logged in succesfully"
+        else
+            flash[:alert] = "Invalid email or password"
+            render :new, status: :unprocessable_entity
+        end
+    end
+
     def destroy
         session[:user_id] = nil
         redirect_to root_path, notice: "Logged Out"
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,10 @@ Rails.application.routes.draw do
 
   get "sign_up", to: "registration#new"
   post "sign_up", to: "registration#create"
+
+  get "sign_in", to: "sessions#new"
+  post "sign_in", to: "sessions#create"
+
   delete "logout", to: "sessions#destroy"
 
   root to: "main#index"
