6806deeff58a454941f8c27f3c5f1d4836183614
ZarakiLancelot <kenpachi2783@gmail.com>
2022-07-10 12:40:12 -0600

    edit password

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,11 +1,15 @@
 class ApplicationController < ActionController::Base
     before_action :set_current_user
-    
+
     def set_current_user
         if session[:user_id]
             Current.user = User.find_by(id: session[:user_id])
             # flash.now[:notice] = "Inicio de sesión exitosa!"
             # flash.now[:alert] = "Correo o contraseña incorrectos!"
         end
+    end
+
+    def require_user_logged_in!
+        redirect_to sign_in_path, alert: "You must be signed in to be here!" if Current.user.nil?
     end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
   get "about", to: "about#index"
 
+  get "password", to: "passwords#edit", as: :edit_password
+  patch "password", to: "passwords#update"
+
   get "sign_up", to: "registration#new"
   post "sign_up", to: "registration#create"
 
