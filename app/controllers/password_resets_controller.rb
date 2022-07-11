c4306fc4cf7d76a4e8319796208313e265f9bbf5
ZarakiLancelot <kenpachi2783@gmail.com>
2022-07-10 22:36:17 -0600

    password reset update

--- app/controllers/password_resets_controller.rb
+++ app/controllers/password_resets_controller.rb
@@ -11,4 +11,25 @@ class PasswordResetsController < ApplicationController
         
         redirect_to root_path, :notice => "Email sent with password reset instructions."
     end
+
+    def edit
+        @user = User.find_signed!(params[:token], purpose: "password_reset")
+    rescue ActiveSupport::MessageVerifier::InvalidSignature
+        redirect_to sign_in_path, :alert => "Invalid or expired password reset link."
+    end
+
+    def update
+        @user = User.find_signed!(params[:token], purpose: "password_reset")
+        if @user.update(password_params)
+            redirect_to sign_in_path, :notice => "Password has been reset."
+        else
+            render :edit, status: :unprocessable_entity
+        end
+    end
+
+    private
+
+    def password_params
+        params.require(:user).permit(:password, :password_confirmation)
+    end
 end
